875c2a3cf40ff9b3bc7ff0b51e2d7ff0cd9cd518
Echeverria, Oscar <oecheverria22@gmail.com>
2025-12-01 15:20:36 -0300

    Adding second app

---
+++ src/main/java/com/oscar/secondapp/secondApp.java
@@ -0,0 +1,11 @@
+package com.oscar.secondapp;
+
+import org.springframework.boot.SpringApplication;
+
+public class secondApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(secondApp.class, args);
+    }
+
+}
